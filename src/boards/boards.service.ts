0baeeb362f723242e45914cf0d9b38cf2f1208ed
joe-jonghyeok <joe@hyper-cloud.kr>
2022-08-02 12:17:05 +0900

    추가: 유효성 체크를 위한 Pipe 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -12,7 +12,11 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find(board => board.id === id);
+    const found = this.boards.find(board => board.id === id);
+    if (!found) {
+      throw new NotFoundException(`Cannot find Board with id ${id}`);
+    }
+    return found;
   }
 
   createBoard(createBoardDto: CreateBoardDto) {
